ff468fe408b29cc5d7d4ff3d479656f998f1afb4
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-03-28 18:59:13 +1100

    custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,12 +5,19 @@ import ts from "typescript-eslint";
 import react from "eslint-plugin-react/configs/all.js";
 import globals from "globals";
 /*
+ * Potential flag config plugins:
+ * jsdoc
+ * vitest
+ */
+/*
  * Future flat config plugins:
  * promises
  * next.js
- * tailwind
- * react-testing-library
- * total-functions
+ * tailwindcss
+ * jsx-a11y
+ * react-hooks
+ * [react-testing-library]
+ * [total-functions]
  */
 
 export default ts.config(
@@ -32,8 +39,6 @@ export default ts.config(
     },
     settings: { react: { version: "detect" } },
     rules: {
-      "sort-imports": "off",
-      "sort-keys": "off",
       "@typescript-eslint/consistent-type-assertions": [
         "error",
         { assertionStyle: "never" },
@@ -53,6 +58,92 @@ export default ts.config(
         },
       ],
       "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      // TypeScript-specific rules
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "warn",
+      "@typescript-eslint/prefer-readonly-parameter-types": "off",
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/max-params": "warn",
+      "@typescript-eslint/sort-type-constituents": "off",
+      "@typescript-eslint/naming-convention": "off",
+      "@typescript-eslint/no-magic-numbers": "off",
+
+      // React-specific rules
+      "react/no-multi-comp": "warn",
+      "react/forbid-component-props": "warn",
+      "react/jsx-max-depth": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-indent-props": "off",
+      "react/jsx-sort-props": "off",
+      "react/jsx-indent": "off",
+      "react/jsx-no-literals": "off",
+      "react/jsx-one-expression-per-line": "off",
+      "react/function-component-definition": "off",
+      "react/jsx-newline": "off",
+      "react/jsx-filename-extension": "off",
+      "react/jsx-max-props-per-line": "off",
+
+      // Functional programming rules
+      "functional/immutable-data": "warn",
+      "functional/no-classes": "warn",
+      "functional/no-conditional-statements": [
+        "warn",
+        { allowReturningBranches: true },
+      ],
+      "functional/no-expression-statements": [
+        "warn",
+        { ignoreCodePattern: "^set|use|createRoot" },
+      ],
+      "functional/no-let": "warn",
+      "functional/no-loop-statements": "warn",
+      "functional/no-mixed-types": "warn",
+      "functional/no-promise-reject": "warn",
+      "functional/no-return-void": "warn",
+      "functional/no-this-expressions": "warn",
+      "functional/no-throw-statements": "warn",
+      "functional/no-try-statements": "warn",
+      "functional/prefer-property-signatures": "warn",
+      "functional/readonly-type": "warn",
+      "functional/type-declaration-immutability": [
+        // TODO: get this to work
+        "off",
+        { ignoreIdentifierPattern: "^z\\.infer" },
+      ],
+      // TODO: get this to work
+      "functional/prefer-immutable-types": "off",
+      "functional/prefer-tacit": "warn",
+      // TODO: get this to work
+      "functional/functional-parameters": "off",
+
+      // General JavaScript rules
+      "arrow-body-style": "warn",
+      camelcase: "warn",
+      "capitalized-comments": "warn",
+      "id-length": "warn",
+      "max-statements": "warn",
+      "no-inline-comments": "warn",
+      "no-undefined": "warn",
+      "no-unused-vars": "off",
+      "no-use-before-define": "off",
+      "no-shadow": "off",
+      "max-lines-per-function": "off",
+      "init-declarations": "off",
+      "no-unused-expressions": "off",
+      "no-multi-str": "off",
+      "max-params": "off",
+      "multiline-comment-style": "off",
+      "sort-imports": "off",
+      "sort-keys": "off",
+      "no-ternary": "off",
+      "no-magic-numbers": "off",
+      "line-comment-position": "off",
+      "no-duplicate-imports": "off",
+      "sort-vars": "off",
+      "no-warning-comments": "off",
+      curly: "off",
+      "one-var": "off",
+      "no-extra-boolean-cast": "off",
+      "dot-notation": "off",
     },
   },
 );
