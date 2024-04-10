2dd28874f7238f8adb8a09b8cacb0cda4352b266
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-04-10 20:12:06 +1000

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,24 @@
 import js from "@eslint/js";
 import fn from "eslint-plugin-functional/flat";
 import ts from "typescript-eslint";
-// @ts-expect-error: React isn't typed yet
-import react from "eslint-plugin-react/configs/all.js";
+import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
-/*
- * Potential flag config plugins:
- * jsdoc
- * vitest
- */
-/*
- * Future flat config plugins:
- * promises
- * next.js
- * tailwindcss
- * jsx-a11y
- * react-hooks
- * [react-testing-library]
- * [total-functions]
- */
+// import reactRecommended from "eslint-plugin-react/configs/all.js";
 
 export default ts.config(
   js.configs.all,
   fn.configs.all,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  react,
   ...ts.configs.strictTypeChecked,
+  jsdoc.configs["flat/recommended"],
+  // reactRecommended,
   {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     languageOptions: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      ...react.languageOptions,
       parserOptions: {
         project: true,
         tsconfigRootDir: import.meta.dirname,
       },
       globals: { ...globals.browser },
     },
-    settings: { react: { version: "detect" } },
     rules: {
       // Custom rule configs from the 'typed-fp' plugin
       "@typescript-eslint/consistent-type-assertions": [
@@ -65,21 +46,6 @@ export default ts.config(
       "@typescript-eslint/sort-type-constituents": "off",
       "@typescript-eslint/naming-convention": "off",
       "@typescript-eslint/no-magic-numbers": "off",
-
-      // React-specific rules
-      "react/no-multi-comp": "warn",
-      "react/forbid-component-props": "warn",
-      "react/jsx-max-depth": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-indent-props": "off",
-      "react/jsx-sort-props": "off",
-      "react/jsx-indent": "off",
-      "react/jsx-no-literals": "off",
-      "react/jsx-one-expression-per-line": "off",
-      "react/function-component-definition": "off",
-      "react/jsx-newline": "off",
-      "react/jsx-filename-extension": "off",
-      "react/jsx-max-props-per-line": "off",
 
       // Functional programming rules
       "functional/immutable-data": "warn",
